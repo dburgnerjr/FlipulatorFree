6559794ee29c678b804b21e47eeb314f9207197f
Daniel Burgner <dburgnerjr@yahoo.com>
2015-10-03 00:43:55 -0400

    Resolved technical issues.
    Resolved technical issues pushing APK to Play Store.

--- gen/com/android/vending/billing/IInAppBillingService.java
+++ gen/com/android/vending/billing/IInAppBillingService.java
@@ -1,6 +1,6 @@
 /*
  * This file is auto-generated.  DO NOT MODIFY.
- * Original file: C:\\Users\\Daniel\\workspace\\FlipulatorFree\\src\\com\\android\\vending\\billing\\IInAppBillingService.aidl
+ * Original file: C:\\Users\\Daniel\\git\\FlipulatorFree\\src\\com\\android\\vending\\billing\\IInAppBillingService.aidl
  */
 package com.android.vending.billing;
 /**
